cf062618be764e0d0e5564bb83b308919dd07a30
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-14 09:27:50 +0000

    Mar 14, 2023, 2:57 PM

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    extends: "plugin:markdown/recommended"
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
 }
