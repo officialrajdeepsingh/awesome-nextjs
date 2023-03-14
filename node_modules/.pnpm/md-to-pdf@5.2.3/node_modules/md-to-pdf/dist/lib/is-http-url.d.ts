711969858cb65894ad9886e3348c74d68f5d191d
Rajdeep singh <32416397+officialrajdeepsingh@users.noreply.github.com>
2023-03-14 17:04:53 +0530

    Delete node_modules directory

--- node_modules/.pnpm/md-to-pdf@5.2.3/node_modules/md-to-pdf/dist/lib/is-http-url.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Check whether the input is a url.
- *
- * @returns `true` if a URL can be constructed from `input`, `false` otherwise.
- */
-export declare const isHttpUrl: (input: string) => boolean;
--- node_modules/.pnpm/md-to-pdf@5.2.3/node_modules/md-to-pdf/dist/lib/is-md-file.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Check whether a path is a markdown file.
- */
-export declare const isMdFile: (path: string) => boolean;
